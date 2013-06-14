b517f2b75422bc44e2a7c5343583bb10d19c64cb
Nathaniel Watts <reg@nathanielwatts.com>
2013-06-13 21:53:54 -0500

    initial commit, new rails 3.2.13 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BackbonerailsDemo::Application.config.secret_token = 'f2de3d624564bce51b7c3421b4876eb20a992f7f103657c8c50933ef2e896a1a87dcb4e0c345d9f6e2f0c92bd269cb28855aee10b7e92b8c9c60d5a03e9749cf'
